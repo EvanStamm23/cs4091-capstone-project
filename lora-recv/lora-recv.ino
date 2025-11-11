80fb6925d692229174fe29d8f97855fb89dee2da
Evan S <evanstamm23@gmail.com>
2025-11-11 15:22:30 -0600

    updated test files to set pins correctly

--- lora-recv/lora-recv.ino
+++ lora-recv/lora-recv.ino
@@ -1,26 +1,42 @@
 #include <SPI.h>
 #include <LoRa.h>
 
+#define SCK   5
+#define MISO  19
+#define MOSI  27
+#define NSS   18
+#define RST   14
+#define DIO0  26
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
-  Serial.println("Starting LoRa Reciever");
 
-  if (!LoRa.begin(915E6)) {
+  Serial.println("LoRa Receiver");
+
+  // Initialize SPI with correct pins
+  SPI.begin(SCK, MISO, MOSI, NSS);
+  
+  // Configure LoRa pins
+  LoRa.setPins(NSS, RST, DIO0);
+
+  // Initialize LoRa at 433 MHz
+  if (!LoRa.begin(433E6)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
+
+  Serial.println("LoRa initialized successfully.");
 }
 
 void loop() {
   int packetSize = LoRa.parsePacket();
   if (packetSize) {
-    String incoming = "";
+    Serial.print("Received packet: ");
     while (LoRa.available()) {
-      incoming += (char)LoRa.read();
+      Serial.print((char)LoRa.read());
     }
-
-  Serial.print("Received: ");
-  Serial.println(incoming);
+    Serial.print(" | RSSI: ");
+    Serial.println(LoRa.packetRssi());
+  }
 }
--- lora/lora.ino
+++ lora/lora.ino
@@ -1,20 +1,38 @@
 #include <SPI.h>
 #include <LoRa.h>
 
+#define SCK   5
+#define MISO  19
+#define MOSI  27
+#define NSS   18
+#define RST   14
+#define DIO0  26
+
+int packet_num = 0;
+
 void setup() {
   Serial.begin(115200);
   while (!Serial);
   Serial.println("Starting LoRa Transmitter");
   
-  LoRa.begin(433E6); //our board is 433 mHz
+  SPI.begin(SCK, MISO, MOSI, NSS);
+  LoRa.setPins(NSS, RST, DIO0);
+
+  if (!LoRa.begin(433E6)) { //our board is 433 mHz
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  Serial.println("LoRa transmitter initialized successfully.");
 }
 
 void loop() {
-  Serial.println("Sending packet...");
+  Serial.println("Sending packet " + String(packet_num));
 
+  String message = "Hello World! Packet #" + String(packet_num);
   LoRa.beginPacket();
-  LoRa.print("Hello World");
+  LoRa.print(message);
   LoRa.endPacket();
-
+  packet_num++;
   delay(2000);
 }
