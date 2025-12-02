915927f307446bb9e60a80e5d08e5985fef02361
Evan S <evanstamm23@gmail.com>
2025-12-02 16:53:22 -0600

    temp display update

--- lora-node-master/lora-node-master.ino
+++ lora-node-master/lora-node-master.ino
@@ -108,15 +108,22 @@ void loop() {
   }
 }
 
-void updateDisplay(int rssiValue){
+void updateDisplay(int rssiValue, String sender){
   display.clearDisplay();
   display.setTextColor(WHITE);
   display.setTextSize(1);
   display.setCursor(0,0);
   display.print("Device: ");
   display.println("Master");
-  display.setCursor(0,20);
-  display.print("RSSI of Sender: ");
+  if(sender == String(CLIENT1_ID, HEX)){
+    display.setCursor(0,20);
+    display.print("RSSI of " + sender + " ");
+  }
+  else if (sender == String(CLIENT2_ID, HEX)){
+    display.setCursor(0,40);
+    display.print("RSSI of " + sender + " ");
+  }
+
   display.println(rssiValue);
   display.display();
 }
@@ -177,7 +184,7 @@ void receiveMessage(int packetSize){
 
   //On device screen
   if (millis() - lastUpdate > 200) {  // update every second to avoid constant display calls
-    updateDisplay(LoRa.packetRssi());
+    updateDisplay(LoRa.packetRssi(), String(sender, HEX));
     lastUpdate = millis();
   }
 }
